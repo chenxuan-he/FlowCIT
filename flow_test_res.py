2701c717e9519a6dca0cfa65b17f226fa5cf32a8
chenxuan-he <chenxuan.stat@gmail.com>
2024-11-28 13:28:14 +0800

    Trial.

--- flow_test_res.py
+++ flow_test_res.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 # Load a DataFrame from a .pkl file
-df = pd.read_pickle('flow_test_res/10_10_10simulation_results.pkl')
+df = pd.read_pickle('flow_test_res/10_10_3simulation_results.pkl')
 
-print(df)
+sum(1 for value in df[1] if value > 0.05)
+
+sum(1 for value in df[3] if value > 0.05)
+
+sum(1 for value in df[5] if value > 0.05)
