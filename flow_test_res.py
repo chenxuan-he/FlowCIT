3b97c905dc52104f0b651cf2a834cb938b37b192
chenxuan-he <chenxuan.stat@gmail.com>
2025-01-21 22:43:52 +0800

    Back up.

--- flow_test_res.py
+++ flow_test_res.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-sim_type=2
-alpha=-.1
+sim_type=1
+alpha=-.2
 # Specify the file name
 file_name = 'results/sim_type' + str(sim_type) + '-alpha-' + str(alpha) + '-n-500-x-3-y-3-z-3.csv'
 
--- sim.sh
+++ sim.sh
@@ -16,8 +16,8 @@ nohup python -u sim.py --gpu=3 --cpu=80-120 --sim_type=1 --p=3 --q=3 --d=3 --n=1
 nohup python -u sim.py --gpu=5 --cpu=120-160 --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=.1 --par_task=5 --nsim=100 &> sim_n1000_s1_2.txt &
 nohup python -u sim.py --gpu=6 --cpu=160-192 --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=.05 --par_task=5 --nsim=100 &> sim_n1000_s1_1.txt &
 nohup python -u sim.py --gpu=7 --cpu=192-224 --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=-.05 --par_task=5 --nsim=100 &> sim_n1000_s1_-1.txt &
-nohup python -u sim.py --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=-.1 --par_task=5 --nsim=100 &> sim_n1000_s1_-2.txt &
-nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 &> sim_n1000_s2_0.txt &
+nohup python -u sim.py --gpu=0 --cpu=0-40 --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=-.1 --par_task=5 --nsim=100 &> sim_n1000_s1_-2.txt &
+nohup python -u sim.py --gpu=1 --cpu=40-80 --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 &> sim_n1000_s2_0.txt &
 nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=.1 --par_task=5 --nsim=100 &> sim_n1000_s2_2.txt &
 nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=.05 --par_task=5 --nsim=100 &> sim_n1000_s2_1.txt &
 nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=-.05 --par_task=5 --nsim=100 &> sim_n1000_s2_-1.txt &
