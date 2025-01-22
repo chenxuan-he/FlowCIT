bdad60c04394c80119f432591f7d7c0cda4701e6
chenxuan-he <chenxuan.stat@gmail.com>
2025-01-22 15:02:00 +0800

    Back up.

--- flow_test_res.py
+++ flow_test_res.py
@@ -1,16 +1,54 @@
+# import pandas as pd
+
+# sim_type=2
+# alphas=[-.2, .1, .0, .1, .2]
+# n=500
+
+# for alpha in alphas:
+#     # Specify the file name
+#     file_name = 'results/sim_type' + str(sim_type) + '-alpha-' + str(alpha) + '-n-' + str(n) + '-x-3-y-3-z-3.csv'
+
+#     # Read the CSV file into a DataFrame
+#     data = pd.read_csv(file_name, header=None)
+
+#     # Calculate the mean of each column
+#     column_means = (data>.05).mean()
+
+#     # Print the column means
+#     print(column_means)
+
+
 import pandas as pd
 
-sim_type=2
-alpha=.0
-n=1000
-# Specify the file name
-file_name = 'results/sim_type' + str(sim_type) + '-alpha-' + str(alpha) + '-n-' + str(n) + '-x-3-y-3-z-3.csv'
+sim_type = 2
+# alphas = [-0.2, 0.1, 0.0, 0.1, 0.2]
+# n = 500
 
-# Read the CSV file into a DataFrame
-data = pd.read_csv(file_name, header=None)
+alphas = [-0.1, -0.05, 0.0, 0.05, 0.1]
+n = 1000
 
-# Calculate the mean of each column
-column_means = (data>.05).mean()
+# Create an empty list to store the means
+means_list = []
 
-# Print the column means
-print(column_means)
+for alpha in alphas:
+    # Specify the file name
+    file_name = f'results/sim_type{sim_type}-alpha-{alpha}-n-{n}-x-3-y-3-z-3.csv'
+
+    # Read the CSV file into a DataFrame
+    data = pd.read_csv(file_name, header=None)
+
+    # Calculate the mean of each column
+    column_means = (data > 0.05).mean()
+
+    # Append the means to the list along with the alpha value
+    means_list.append(column_means)
+
+# Convert the list of means into a DataFrame
+means_df = pd.DataFrame(means_list, index=alphas)
+
+# Optionally, you can reset the index to have a column for alpha
+means_df.reset_index(inplace=True)
+means_df.rename(columns={'index': 'alpha'}, inplace=True)
+
+# Print the resulting DataFrame
+print(means_df)
--- sim.sh
+++ sim.sh
@@ -12,6 +12,7 @@ nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=500 --alpha=.1 --par_t
 nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=500 --alpha=-.1 --par_task=5 --nsim=100 &> sim_n500_s2_-1.txt &
 nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=500 --alpha=-.2 --par_task=5 --nsim=100 &> sim_n500_s2_-2.txt &
 
+
 # series of (d_x, d_y, d_z, n)=(3, 3, 3, 1000)
 nohup python -u sim.py --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 &> sim_n1000_s1_0.txt &
 nohup python -u sim.py --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=.1 --par_task=5 --nsim=100 &> sim_n1000_s1_2.txt &
@@ -21,12 +22,9 @@ nohup python -u sim.py --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=-.1 --par
 nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 &> sim_n1000_s2_0.txt &
 nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=-.05 --par_task=5 --nsim=100 &> sim_n1000_s2_-1.txt &
 nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=-.1 --par_task=5 --nsim=100 &> sim_n1000_s2_-2.txt &
-
-# running on the server 17:
-nohup python -u sim.py --cpu=0-128 --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=.1 --par_task=100 --nsim=100 &> sim_n1000_s2_2.txt &
-nohup python -u sim.py --cpu=128-256 --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=.05 --par_task=100 --nsim=100 &> sim_n1000_s2_1.txt &
-
-# try sim 2 H_0 under different hidden_num
+nohup python -u sim.py --p=3 --q=3 --d=3 --n=1000 --alpha=.1 --par_task=100 --nsim=100 &> sim_n1000_s2_2.txt &
+nohup python -u sim.py --p=3 --q=3 --d=3 --n=1000 --alpha=.05 --par_task=100 --nsim=100 &> sim_n1000_s2_1.txt &
+# try sim 2 H_0 under different hidden_num, similar results
 nohup python -u sim.py --gpu=0 --cpu=0-100 --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 --hidden_num=32 &> sim_n1000_s2_0_1.txt &
 nohup python -u sim.py --gpu=1 --cpu=100-200 --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 --hidden_num=128 &> sim_n1000_s2_0_2.txt &
 
