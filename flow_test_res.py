cfc7e5dc1d7f97cd2d6058f115723ea06e660a39
chenxuan-he <chenxuan.stat@gmail.com>
2025-01-22 11:14:44 +0800

    Back up.

--- flow_test_res.py
+++ flow_test_res.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-sim_type=1
-alpha=-.2
+sim_type=2
+alpha=.0
+n=1000
 # Specify the file name
-file_name = 'results/sim_type' + str(sim_type) + '-alpha-' + str(alpha) + '-n-500-x-3-y-3-z-3.csv'
+file_name = 'results/sim_type' + str(sim_type) + '-alpha-' + str(alpha) + '-n-' + str(n) + '-x-3-y-3-z-3.csv'
 
 # Read the CSV file into a DataFrame
 data = pd.read_csv(file_name, header=None)
--- sim.sh
+++ sim.sh
@@ -12,15 +12,24 @@ nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=500 --alpha=-.1 --par_
 nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=500 --alpha=-.2 --par_task=5 --nsim=100 &> sim_n500_s2_-2.txt &
 
 # series of (d_x, d_y, d_z, n)=(3, 3, 3, 1000)
-nohup python -u sim.py --gpu=3 --cpu=80-120 --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 &> sim_n1000_s1_0.txt &
-nohup python -u sim.py --gpu=5 --cpu=120-160 --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=.1 --par_task=5 --nsim=100 &> sim_n1000_s1_2.txt &
-nohup python -u sim.py --gpu=6 --cpu=160-192 --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=.05 --par_task=5 --nsim=100 &> sim_n1000_s1_1.txt &
-nohup python -u sim.py --gpu=7 --cpu=192-224 --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=-.05 --par_task=5 --nsim=100 &> sim_n1000_s1_-1.txt &
-nohup python -u sim.py --gpu=0 --cpu=0-40 --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=-.1 --par_task=5 --nsim=100 &> sim_n1000_s1_-2.txt &
-nohup python -u sim.py --gpu=1 --cpu=40-80 --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 &> sim_n1000_s2_0.txt &
-nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=.1 --par_task=5 --nsim=100 &> sim_n1000_s2_2.txt &
-nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=.05 --par_task=5 --nsim=100 &> sim_n1000_s2_1.txt &
+nohup python -u sim.py --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 &> sim_n1000_s1_0.txt &
+nohup python -u sim.py --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=.1 --par_task=5 --nsim=100 &> sim_n1000_s1_2.txt &
+nohup python -u sim.py --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=.05 --par_task=5 --nsim=100 &> sim_n1000_s1_1.txt &
+nohup python -u sim.py --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=-.05 --par_task=5 --nsim=100 &> sim_n1000_s1_-1.txt &
+nohup python -u sim.py --sim_type=1 --p=3 --q=3 --d=3 --n=1000 --alpha=-.1 --par_task=5 --nsim=100 &> sim_n1000_s1_-2.txt &
+nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 &> sim_n1000_s2_0.txt &
 nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=-.05 --par_task=5 --nsim=100 &> sim_n1000_s2_-1.txt &
 nohup python -u sim.py --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=-.1 --par_task=5 --nsim=100 &> sim_n1000_s2_-2.txt &
 
+# running on the server 17:
+nohup python -u sim.py --cpu=0-128 --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=.1 --par_task=100 --nsim=100 &> sim_n1000_s2_2.txt &
+nohup python -u sim.py --cpu=128-256 --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=.05 --par_task=100 --nsim=100 &> sim_n1000_s2_1.txt &
 
+# try sim 2 H_0 under different hidden_num
+nohup python -u sim.py --gpu=0 --cpu=0-100 --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 --hidden_num=32 &> sim_n1000_s2_0_1.txt &
+nohup python -u sim.py --gpu=1 --cpu=100-200 --sim_type=2 --p=3 --q=3 --d=3 --n=1000 --alpha=0 --par_task=5 --nsim=100 --hidden_num=128 &> sim_n1000_s2_0_2.txt &
+
+
+# seris of (d_x, d_y, d_z, n)=(3, 3, 100, 1000)
+nohup python -u sim.py --cpu=0-50 --sim_type=1 --p=3 --q=3 --d=100 --n=1000 --alpha=0 --par_task=5 --nsim=100 &> sim_n1000_s1_d100_0.txt &
+nohup python -u sim.py --cpu=50-100 --sim_type=1 --p=3 --q=3 --d=100 --n=1000 --alpha=.05 --par_task=5 --nsim=100 &> sim_n1000_s1_d100_1.txt &
