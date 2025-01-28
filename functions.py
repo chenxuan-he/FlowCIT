a00fc11f8ce841bfe3d2215cbb1a20848fcd73ad
chenxuan-he <chenxuan.stat@gmail.com>
2025-01-28 13:48:03 +0800

    Back up.

--- functions.py
+++ functions.py
@@ -40,7 +40,7 @@ def generate_data(model=1, sim_type=0, n=1000, p=3, q=3, d=3, alpha=.1, seed=0):
     elif sim_type == 3:
         X = (Z @ beta_1) + torch.randn((n, p))
         # Y = torch.cos(Z @ beta_2) + (X @ beta_3 * alpha) + torch.randn((n, q))
-        Y = torch.cos(Z @ beta_2) + (X @ beta_3 * alpha) + torch.randn((n, q))
+        Y = torch.pow(Z @ beta_2, 2) + (X @ beta_3 * alpha) + torch.randn((n, q))
     elif sim_type == 4:
         X = Z @ beta_1 + torch.randn((n, p))
         Y = Z @ beta_2 + torch.exp(X @ beta_3 * alpha) + torch.randn((n, q))
