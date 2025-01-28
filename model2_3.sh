eac3508ba19254eb82b16d787f9fe703c4511928
chenxuan-he <chenxuan.stat@gmail.com>
2025-01-28 23:58:54 +0800

    Back up.

--- model2_3.sh
+++ model2_3.sh
@@ -5,9 +5,6 @@ nohup python -u model.py --gpu=3 --cpu=0-220 --model=2 --sim_type=3 --p=50 --q=5
 nohup python -u model.py --gpu=4 --cpu=0-220 --model=2 --sim_type=3 --p=50 --q=50 --d=100 --n=1000 --alpha=0.4 --par_task=5 --nsim=100 --hidden_num=32 &> model2_n1000_s3_4.txt & 
 
 
-
-nohup python -u model.py --gpu=3 --cpu=0-50 --model=2 --sim_type=3 --p=50 --q=50 --d=100 --n=1000 --alpha=0 --par_task=5 --nsim=100 --hidden_num=32 &> model2_n1000_s3_0.txt &
-nohup python -u model.py --gpu=4 --cpu=40-90 --model=2 --sim_type=3 --p=50 --q=50 --d=100 --n=1000 --alpha=0.2 --par_task=5 --nsim=100 --hidden_num=32 &> model2_n1000_s3_2.txt & 
-nohup python -u model.py --gpu=5 --cpu=85-135 --model=2 --sim_type=3 --p=50 --q=50 --d=100 --n=1000 --alpha=-0.2 --par_task=5 --nsim=100 --hidden_num=32 &> model2_n1000_s3_-2.txt & 
-nohup python -u model.py --gpu=6 --cpu=130-180 --model=2 --sim_type=3 --p=50 --q=50 --d=100 --n=1000 --alpha=-0.4 --par_task=5 --nsim=100 --hidden_num=32 &> model2_n1000_s3_-4.txt & 
-nohup python -u model.py --gpu=7 --cpu=170-220 --model=2 --sim_type=3 --p=50 --q=50 --d=100 --n=1000 --alpha=0.4 --par_task=5 --nsim=100 --hidden_num=32 &> model2_n1000_s3_4.txt &
+nohup python -u model.py --gpu=4 --cpu=0-220 --model=2 --sim_type=3 --p=50 --q=50 --d=100 --n=1000 --alpha=-.4 --par_task=5 --nsim=100 --hidden_num=32 &> model2_n1000_s3_-4.txt & 
+nohup python -u model.py --gpu=5 --cpu=0-220 --model=2 --sim_type=3 --p=50 --q=50 --d=100 --n=1000 --alpha=-0.8 --par_task=5 --nsim=100 --hidden_num=32 &> model2_n1000_s3_-8.txt & 
+nohup python -u model.py --gpu=6 --cpu=0-220 --model=2 --sim_type=3 --p=50 --q=50 --d=100 --n=1000 --alpha=0.8 --par_task=5 --nsim=100 --hidden_num=32 &> model2_n1000_s3_8.txt & 
