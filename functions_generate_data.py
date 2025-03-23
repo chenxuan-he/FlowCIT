e711f1503c1f9d84f8cd71d0b806fe311cf276cc
chenxuan-he <chenxuan.stat@gmail.com>
2025-03-23 22:39:56 +0800

    Back up.

--- functions_generate_data.py
+++ functions_generate_data.py
@@ -35,20 +35,19 @@ def generate_data(model=1, sim_type=0, n=1000, p=3, q=3, d=3, alpha=.1, seed=0):
     elif model == 1 and sim_type == 3:
         X = torch.pow(Z @ beta_1, 2) + torch.randn((n, p))
         Y = Z @ beta_2 + X @ beta_3 * alpha + torch.randn((n, q))
-    elif model == 1 and sim_type == 4:
+    elif (model == 1 and sim_type == 4) or (model == 3 and sim_type == 2):
         X = Z @ beta_1 + torch.randn((n, p))
         Y = Z @ beta_2 + torch.exp(X @ beta_3 * alpha) + torch.randn((n, q))
     elif model == 2 and sim_type == 2:
         X = torch.abs(Z @ beta_1) + torch.randn((n, p))
         Y = (Z @ beta_2) + X @ beta_3 * alpha + torch.randn((n, q))
-    elif sim_type == 3:
+    elif model == 2 and sim_type == 3:
         X = (Z @ beta_1) + torch.randn((n, p))
         # Y = torch.cos(Z @ beta_2) + (X @ beta_3 * alpha) + torch.randn((n, q))
         Y = torch.pow(torch.abs(Z @ beta_2), 3/2) + (X @ beta_3 * alpha) + torch.randn((n, q))
-    elif sim_type == 4:
+    elif model == 2 and sim_type == 4:
         X = Z @ beta_1 + torch.randn((n, p))
         Y = Z @ beta_2 + torch.exp(X @ beta_3 * alpha) + torch.randn((n, q))
-
     df = pd.DataFrame(
         torch.cat([X, Y, Z], dim=1).numpy(),
         columns=[f"X{i+1}" for i in range(p)] + [f"Y{i+1}" for i in range(q)] + [f"Z{i+1}" for i in range(d)]
--- model3_1_n500.sh
+++ model3_1_n500.sh
@@ -1,42 +1,42 @@
 # First: generate data
-nohup python model_generate_data.py --model=3 --sim_type=1 --alpha=.00 --nsim=200 --d=2 --p=1 --q=1 --n=500
-nohup python model_generate_data.py --model=3 --sim_type=1 --alpha=.05 --nsim=200 --d=2 --p=1 --q=1 --n=500
-nohup python model_generate_data.py --model=3 --sim_type=1 --alpha=.10 --nsim=200 --d=2 --p=1 --q=1 --n=500
-nohup python model_generate_data.py --model=3 --sim_type=1 --alpha=.15 --nsim=200 --d=2 --p=1 --q=1 --n=500
-nohup python model_generate_data.py --model=3 --sim_type=1 --alpha=.20 --nsim=200 --d=2 --p=1 --q=1 --n=500
+python model_generate_data.py --model=3 --sim_type=1 --alpha=.00 --nsim=200 --d=2 --p=1 --q=1 --n=500
+python model_generate_data.py --model=3 --sim_type=1 --alpha=.10 --nsim=200 --d=2 --p=1 --q=1 --n=500
+python model_generate_data.py --model=3 --sim_type=1 --alpha=.20 --nsim=200 --d=2 --p=1 --q=1 --n=500
+python model_generate_data.py --model=3 --sim_type=1 --alpha=.30 --nsim=200 --d=2 --p=1 --q=1 --n=500
+python model_generate_data.py --model=3 --sim_type=1 --alpha=.40 --nsim=200 --d=2 --p=1 --q=1 --n=500
 
 # # use Rscript to execute the CLZ test and the KCI test
-nohup Rscript model_CLZ.R --model=3 --sim_type=1 --alpha=.00 --n=500 --p=1 --q=1 --d=2 --n_cpu=40 --bandwidth=.2 &> model3_CLZ_s1_a00.txt &
-nohup Rscript model_CLZ.R --model=3 --sim_type=1 --alpha=.05 --n=500 --p=1 --q=1 --d=2 --n_cpu=40 --bandwidth=.2 &> model3_CLZ_s1_a05.txt &
-nohup Rscript model_CLZ.R --model=3 --sim_type=1 --alpha=.10 --n=500 --p=1 --q=1 --d=2 --n_cpu=40 --bandwidth=.2 &> model3_CLZ_s1_a10.txt &
-nohup Rscript model_CLZ.R --model=3 --sim_type=1 --alpha=.15 --n=500 --p=1 --q=1 --d=2 --n_cpu=40 --bandwidth=.2 &> model3_CLZ_s1_a15.txt &
-nohup Rscript model_CLZ.R --model=3 --sim_type=1 --alpha=.20 --n=500 --p=1 --q=1 --d=2 --n_cpu=40 --bandwidth=.2 &> model3_CLZ_s1_a20.txt &
+nohup Rscript model_CLZ.R --model=3 --sim_type=1 --alpha=.00 --n=500 --p=1 --q=1 --d=2 --n_cpu=40 --bandwidth=.5 &> model3_CLZ_s1_a00.txt &
+nohup Rscript model_CLZ.R --model=3 --sim_type=1 --alpha=.10 --n=500 --p=1 --q=1 --d=2 --n_cpu=40 --bandwidth=.5 &> model3_CLZ_s1_a10.txt &
+nohup Rscript model_CLZ.R --model=3 --sim_type=1 --alpha=.20 --n=500 --p=1 --q=1 --d=2 --n_cpu=40 --bandwidth=.5 &> model3_CLZ_s1_a20.txt &
+nohup Rscript model_CLZ.R --model=3 --sim_type=1 --alpha=.30 --n=500 --p=1 --q=1 --d=2 --n_cpu=40 --bandwidth=.5 &> model3_CLZ_s1_a30.txt &
+nohup Rscript model_CLZ.R --model=3 --sim_type=1 --alpha=.40 --n=500 --p=1 --q=1 --d=2 --n_cpu=40 --bandwidth=.5 &> model3_CLZ_s1_a40.txt &
 
 # get result for CLZ and KCI
-Rscript model_CLZ_result.R --model=3 --sim_type=1 --n=500 --p=1 --q=1 --d=2 --bandwidth=.2 --alpha=.00
-Rscript model_CLZ_result.R --model=3 --sim_type=1 --n=500 --p=1 --q=1 --d=2 --bandwidth=.2 --alpha=.05
-Rscript model_CLZ_result.R --model=3 --sim_type=1 --n=500 --p=1 --q=1 --d=2 --bandwidth=.2 --alpha=.10
-Rscript model_CLZ_result.R --model=3 --sim_type=1 --n=500 --p=1 --q=1 --d=2 --bandwidth=.2 --alpha=.15
-Rscript model_CLZ_result.R --model=3 --sim_type=1 --n=500 --p=1 --q=1 --d=2 --bandwidth=.2 --alpha=.20
+Rscript model_CLZ_result.R --model=3 --sim_type=1 --n=500 --p=1 --q=1 --d=2 --bandwidth=.5 --alpha=.00
+Rscript model_CLZ_result.R --model=3 --sim_type=1 --n=500 --p=1 --q=1 --d=2 --bandwidth=.5 --alpha=.10
+Rscript model_CLZ_result.R --model=3 --sim_type=1 --n=500 --p=1 --q=1 --d=2 --bandwidth=.5 --alpha=.20
+Rscript model_CLZ_result.R --model=3 --sim_type=1 --n=500 --p=1 --q=1 --d=2 --bandwidth=.5 --alpha=.30
+Rscript model_CLZ_result.R --model=3 --sim_type=1 --n=500 --p=1 --q=1 --d=2 --bandwidth=.5 --alpha=.40
 
 # # python to execute our proposed test, CDC test, FCIT test
 nohup python -u model.py --model=3 --sim_type=1 --alpha=.00 --n=500 --p=1 --q=1 --d=2 --par_task=5 --gpu=1 --cpu=000-040 --nsim=200 &> model3_s1_a00.txt &
-nohup python -u model.py --model=3 --sim_type=1 --alpha=.05 --n=500 --p=1 --q=1 --d=2 --par_task=5 --gpu=2 --cpu=040-080 --nsim=200 &> model3_s1_a05.txt &
-nohup python -u model.py --model=3 --sim_type=1 --alpha=.10 --n=500 --p=1 --q=1 --d=2 --par_task=5 --gpu=3 --cpu=080-120 --nsim=200 &> model3_s1_a10.txt &
-nohup python -u model.py --model=3 --sim_type=1 --alpha=.15 --n=500 --p=1 --q=1 --d=2 --par_task=5 --gpu=4 --cpu=120-160 --nsim=200 &> model3_s1_a15.txt &
-nohup python -u model.py --model=3 --sim_type=1 --alpha=.20 --n=500 --p=1 --q=1 --d=2 --par_task=5 --gpu=5 --cpu=160-200 --nsim=200 &> model3_s1_a20.txt &
+nohup python -u model.py --model=3 --sim_type=1 --alpha=.10 --n=500 --p=1 --q=1 --d=2 --par_task=5 --gpu=2 --cpu=040-080 --nsim=200 &> model3_s1_a10.txt &
+nohup python -u model.py --model=3 --sim_type=1 --alpha=.20 --n=500 --p=1 --q=1 --d=2 --par_task=5 --gpu=3 --cpu=080-120 --nsim=200 &> model3_s1_a20.txt &
+nohup python -u model.py --model=3 --sim_type=1 --alpha=.30 --n=500 --p=1 --q=1 --d=2 --par_task=5 --gpu=4 --cpu=120-160 --nsim=200 &> model3_s1_a30.txt &
+nohup python -u model.py --model=3 --sim_type=1 --alpha=.40 --n=500 --p=1 --q=1 --d=2 --par_task=5 --gpu=5 --cpu=160-200 --nsim=200 &> model3_s1_a40.txt &
 
-python model_result.py --model=3 --sim_type=1 --alphas="0.0,0.05,0.1,0.15,0.2" --n=500 --p=1 --q=1 --d=2 --hidden_num=64 
+python model_result.py --model=3 --sim_type=1 --alphas="0.0,0.1,0.2,0.3,0.4" --n=500 --p=1 --q=1 --d=2 --hidden_num=64 
 
 # # conda activate py37
 # # python code to execute GCIT
-nohup python -u model_GCIT.py --model=3 --sim_type=1 --alpha=.00 --n=500 --p=1 --q=1 --d=2 --par_task=50 --cpu=000-040 --nsim=200 --test_prop=.02 &> model3_s1_GCIT_a00.txt &
-nohup python -u model_GCIT.py --model=3 --sim_type=1 --alpha=.05 --n=500 --p=1 --q=1 --d=2 --par_task=50 --cpu=040-080 --nsim=200 --test_prop=.02 &> model3_s1_GCIT_a05.txt &
-nohup python -u model_GCIT.py --model=3 --sim_type=1 --alpha=.10 --n=500 --p=1 --q=1 --d=2 --par_task=50 --cpu=080-120 --nsim=200 --test_prop=.02 &> model3_s1_GCIT_a10.txt &
-nohup python -u model_GCIT.py --model=3 --sim_type=1 --alpha=.15 --n=500 --p=1 --q=1 --d=2 --par_task=50 --cpu=120-160 --nsim=200 --test_prop=.02 &> model3_s1_GCIT_a15.txt &
-nohup python -u model_GCIT.py --model=3 --sim_type=1 --alpha=.20 --n=500 --p=1 --q=1 --d=2 --par_task=50 --cpu=160-200 --nsim=200 --test_prop=.02 &> model3_s1_GCIT_a20.txt &
+nohup python -u model_GCIT.py --model=3 --sim_type=1 --alpha=.00 --n=500 --p=1 --q=1 --d=2 --par_task=50 --cpu=000-040 --nsim=200 --test_prop=.01 &> model3_s1_GCIT_a00.txt &
+nohup python -u model_GCIT.py --model=3 --sim_type=1 --alpha=.10 --n=500 --p=1 --q=1 --d=2 --par_task=50 --cpu=040-080 --nsim=200 --test_prop=.01 &> model3_s1_GCIT_a10.txt &
+nohup python -u model_GCIT.py --model=3 --sim_type=1 --alpha=.20 --n=500 --p=1 --q=1 --d=2 --par_task=50 --cpu=080-120 --nsim=200 --test_prop=.01 &> model3_s1_GCIT_a20.txt &
+nohup python -u model_GCIT.py --model=3 --sim_type=1 --alpha=.30 --n=500 --p=1 --q=1 --d=2 --par_task=50 --cpu=120-160 --nsim=200 --test_prop=.01 &> model3_s1_GCIT_a30.txt &
+nohup python -u model_GCIT.py --model=3 --sim_type=1 --alpha=.40 --n=500 --p=1 --q=1 --d=2 --par_task=50 --cpu=160-200 --nsim=200 --test_prop=.01 &> model3_s1_GCIT_a40.txt &
 
-python model_result.py --model=3 --sim_type=1 --alphas="0.0,0.05,0.1,0.15,0.2" --n=500 --p=1 --q=1 --d=2 --hidden_num=64 --GCIT=True --GCIT_test_prop=.02
+python model_result.py --model=3 --sim_type=1 --alphas="0.0,0.1,0.2,0.3,0.4" --n=500 --p=1 --q=1 --d=2 --hidden_num=64 --GCIT=True --GCIT_test_prop=.01
 
 # Generate plots
-python model_plot.py --model=3 --sim_type=1 --n=500 --alphas="0.0,0.05,0.1,0.15,0.2" --legend=0
+python model_plot.py --model=3 --sim_type=1 --n=500 --alphas="0.0,0.1,0.2,0.3,0.4" --legend=0
