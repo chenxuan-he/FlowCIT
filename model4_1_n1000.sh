41a4e99b93fabe9b95e558fe6b3b733b3e75569e
chenxuan-he <chenxuan.stat@gmail.com>
2025-04-04 22:08:12 +0800

    Back up.

--- model4_1_n1000.sh
+++ model4_1_n1000.sh
@@ -18,7 +18,7 @@ nohup python -u model.py --model=4 --FlowCIT=0 --FCIT=1 --CDC=0 --CCIT=1 --sim_t
 nohup python -u model.py --model=4 --FlowCIT=0 --FCIT=1 --CDC=0 --CCIT=1 --sim_type=1 --alpha=.30 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --cpu=040-255 --gpu="" --nsim=200 &> model4_CCIT_s1_a30.txt &
 nohup python -u model.py --model=4 --FlowCIT=0 --FCIT=1 --CDC=0 --CCIT=1 --sim_type=1 --alpha=.40 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --cpu=040-255 --gpu="" --nsim=200 &> model4_CCIT_s1_a40.txt &
 
-python model_result.py --model=4 --sim_type=1 --alphas="0.0,0.1,0.2,0.3,0.4" --n=1000 --p=5 --q=5 --d=50 --hidden_num=16
+python model_result.py --model=4 --sim_type=1 --alphas="0.0,0.1,0.2,0.3,0.4" --n=1000 --p=5 --q=5 --d=50
 
 # # # conda activate py37
 # # # python code to execute GCIT
