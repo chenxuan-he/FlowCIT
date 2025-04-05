990e41dfcbb013e6a8f3e19746df00fbb8d0a447
chenxuan-he <chenxuan.stat@gmail.com>
2025-04-05 20:45:59 +0800

    Back up.

--- model4_4_n1000.sh
+++ model4_4_n1000.sh
@@ -6,20 +6,12 @@ python model_generate_data.py --model=4 --sim_type=4 --alpha=.30 --nsim=200 --d=
 python model_generate_data.py --model=4 --sim_type=4 --alpha=.40 --nsim=200 --d=50 --p=5 --q=5 --n=1000
 
 # 2. Python code to execute FlowCIT, CDC test, FCIT test, and CCIT test
-nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=1 --FCIT=0 --CDC=0 --CCIT=0 --hidden_num=16 --alpha=.00 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=6 --cpu=000-220 --nsim=200 &> model4_s4_a00.txt &
-nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=1 --FCIT=0 --CDC=0 --CCIT=0 --hidden_num=16 --alpha=.10 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=1 --cpu=000-220 --nsim=200 &> model4_s4_a10.txt &
-nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=1 --FCIT=0 --CDC=0 --CCIT=0 --hidden_num=16 --alpha=.20 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=2 --cpu=000-220 --nsim=200 &> model4_s4_a20.txt &
-nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=1 --FCIT=0 --CDC=0 --CCIT=0 --hidden_num=16 --alpha=.30 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=3 --cpu=000-220 --nsim=200 &> model4_s4_a30.txt &
-nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=1 --FCIT=0 --CDC=0 --CCIT=0 --hidden_num=16 --alpha=.40 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=4 --cpu=000-220 --nsim=200 &> model4_s4_a40.txt &
+nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=1 --FCIT=1 --CDC=1 --CCIT=1 --hidden_num=16 --alpha=.00 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=6 --cpu=000-220 --nsim=200 &> model4_s4_a00.txt &
+nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=1 --FCIT=1 --CDC=1 --CCIT=1 --hidden_num=16 --alpha=.10 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=1 --cpu=000-220 --nsim=200 &> model4_s4_a10.txt &
+nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=1 --FCIT=1 --CDC=1 --CCIT=1 --hidden_num=16 --alpha=.20 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=2 --cpu=000-220 --nsim=200 &> model4_s4_a20.txt &
+nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=1 --FCIT=1 --CDC=1 --CCIT=1 --hidden_num=16 --alpha=.30 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=3 --cpu=000-220 --nsim=200 &> model4_s4_a30.txt &
+nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=1 --FCIT=1 --CDC=1 --CCIT=1 --hidden_num=16 --alpha=.40 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=4 --cpu=000-220 --nsim=200 &> model4_s4_a40.txt &
 python model_result.py --model=4 --sim_type=4 --alphas="0.0,0.1,0.2,0.3,0.4" --n=1000 --p=5 --q=5 --d=50 --hidden_num=16
-
-nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=0 --FCIT=1 --CDC=1 --CCIT=1 --hidden_num=16 --alpha=.00 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=6 --cpu=000-220 --nsim=200 &> model4_s4_a00.txt &
-nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=0 --FCIT=1 --CDC=1 --CCIT=1 --hidden_num=16 --alpha=.10 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=1 --cpu=000-220 --nsim=200 &> model4_s4_a10.txt &
-nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=0 --FCIT=1 --CDC=1 --CCIT=1 --hidden_num=16 --alpha=.20 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=2 --cpu=000-220 --nsim=200 &> model4_s4_a20.txt &
-nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=0 --FCIT=1 --CDC=1 --CCIT=1 --hidden_num=16 --alpha=.30 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=3 --cpu=000-220 --nsim=200 &> model4_s4_a30.txt &
-nohup python -u model.py --model=4 --sim_type=4 --FlowCIT=0 --FCIT=1 --CDC=1 --CCIT=1 --hidden_num=16 --alpha=.40 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=4 --cpu=000-220 --nsim=200 &> model4_s4_a40.txt &
-python model_result.py --model=4 --sim_type=4 --alphas="0.0,0.1,0.2,0.3,0.4" --n=1000 --p=5 --q=5 --d=50 --hidden_num=16
-
 
 # 3. Python code to execute FlowCIT-IPC
 nohup python -u model.py --model=4 --sim_type=4 --alpha=.0 --n=1000 --p=5 --q=5 --d=50 --par_task=5 --gpu=0 --cpu=000-222 --nsim=200 --CCIT=0 --CDC=0 --FCIT=0 --FlowCIT=1 --FlowCIT_method="IPC" --FlowCIT_permutation=0 --hidden_num=16 &> model4_s4_a00.txt &
